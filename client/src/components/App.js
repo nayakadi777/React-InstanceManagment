d31535ab8c15abc83722f79d16f1e5d2155a0ca7
Adity G Nayak <aditya.gnayak@ltts.com>
2020-11-26 22:57:14 +0530

    app.js file updated

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,65 +1,23 @@
 import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import Header from './HeaderComponents/HeaderComponents.jsx';
-import InstanceList from './InstanceList/InstanceList.js'
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import '../index.css';
+import logo from "../logo.svg";
 
-const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-    border:2 ,
-    margin:50,
-    borderRadius: 10,
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-});
 function App() {
-  const classes = useStyles();
   return (
-    <div className="main">
-     <Header />
-     <Card className={classes.root}>
-      <CardContent className="topCard">
-        <div>
-          <div>
-          AAAA
-          </div>
-          <div>
-            BBB
-          </div>
-        </div>
-        <div>
-          CC
-        </div>
-      </CardContent>
-      {/* <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions> */}
-    </Card>
-     <Card className={classes.root}>
-     <CardContent>
-        <Typography variant="h5" component="h2">
-          Instance
-        </Typography>
-      </CardContent>
-      <CardContent>
-      <InstanceList />
-      </CardContent>
-    </Card>
-     
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
